87aa2c7ca99fa15ac2c11a4fa3e9a674c1dad6ff
Mohammed Dawood <mohammeddawood@Mohammed-Dawood.local>
2020-12-18 10:09:29 +0100

    labb 7

---
+++ lab 7/script.js
@@ -0,0 +1,34 @@
+var x = 10 ;
+var y = 5 ;
+var z = x + y ;
+var w = x - y ;
+var a = x * y ;
+
+
+console.log ("Number1 + Number2 = " + z);
+console.log ("Number1 - Number2 = " + w);
+console.log ("Number1 * Number2 = " + a);
+
+
+
+
+// -------------------------------------------------
+
+
+var förNamn = "Mohammed";
+var efterNamn = "Dawood";
+var ålder = 40;
+
+console.log ("Hej, Mitt namn är " + förNamn + " " + efterNamn + " och jag är " +" ( " + ålder + " )"+ " år gammal. ");
+
+
+
+
+
+
+
+
+
+
+
+
